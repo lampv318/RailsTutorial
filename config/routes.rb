b0cebdd1eea1e0e81320ae620703b809df530189
lampv <lampvhy@gmail.com>
2017-11-12 17:59:43 +0700

    sign up

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get "/contact", to: "static_pages#contact"
   get "/about", to: "static_pages#about"
   get "/signup", to: "users#new"
+  post "/signup",  to: "users#create"
+  resources :users
 end
